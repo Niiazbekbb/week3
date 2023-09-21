d5e7afa1ff3172e6c391b51bec85c4a56e598ef3
Niiazbekbb <144800349+Niiazbekbb@users.noreply.github.com>
2023-09-22 01:01:45 +0900

    Create test3.py

---
+++ test3.py
@@ -0,0 +1,19 @@
+count = 0
+total = 0
+while True:
+    user_input = input("Enter a number: ")
+    if user_input == "done":
+        break
+    try:
+        number = float(user_input)
+        total += number
+        count += 1
+    except ValueError:
+        print("Invalid input. Enter a number.")
+if count > 0:
+    average = total / count
+    print("Sum of input numbers:", total)
+    print("Number of input:", count)
+    print("Average of input numbers:", average)
+else:
+    print("No numbers entered.")
